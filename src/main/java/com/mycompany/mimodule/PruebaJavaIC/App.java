6d5a69c4dcf84450b4eaf91e33ac9fea61843984
Leo Antoli <leo.antoli@gmail.com>
2014-01-15 15:18:16 +0100

    initial version

---
+++ src/main/java/com/mycompany/mimodule/PruebaJavaIC/App.java
@@ -0,0 +1,12 @@
+package com.mycompany.mimodule.PruebaJavaIC;
+
+/**
+ * Hello world!
+ * 
+ */
+public class App {
+	public static void main(String[] args) {
+		int a = 10;
+		System.out.println("Hello World!");
+	}
+}
